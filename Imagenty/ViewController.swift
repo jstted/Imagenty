b3d08127179d207b42a8fc5c7a0955e8c72505bb
jstted <tedeev17@gmail.com>
2022-12-19 12:16:09 +0300

    Initial Commit

---
+++ Imagenty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Imagenty
+//
+//  Created by Mikhail Tedeev on 19.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
