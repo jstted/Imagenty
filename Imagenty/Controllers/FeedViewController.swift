72fbf19c8dda995f612b1d6dfbf1039f91a4f879
jstted <tedeev17@gmail.com>
2022-12-19 14:39:21 +0300

    Created

--- Imagenty/Controllers/FeedViewController.swift
+++ Imagenty/Controllers/FeedViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FeedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        self.tabBarController.
     }
-
+    
 
 }
 
---
+++ Imagenty/Delegate/SceneDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  SceneDelegate.swift
+//  Imagenty
+//
+//  Created by Mikhail Tedeev on 19.12.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        
+        let feedViewController = FeedViewController()
+        let likedViewController = LikedViewController()
+        let tabBarItems = [feedViewController, likedViewController]
+        
+        let tabBarController = UITabBarController()
+        tabBarController.setViewControllers(tabBarItems, animated: true)
+        
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
+        
+    }
+
+}
+
